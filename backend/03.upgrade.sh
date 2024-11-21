1cafba160d7ea9d3582b89979757e231ebff6300
River Xie <xiehust@163.com>
2024-11-21 09:41:23 +0000

    fix updaet

--- backend/03.upgrade.sh
+++ backend/03.upgrade.sh
@@ -5,6 +5,8 @@ git stash
 cd /home/ubuntu/llm_model_hub/backend/
 git pull
 git submodule update --remote
+source ../miniconda3/bin/activate py311
+pip install -r requirements.txt
 cd /home/ubuntu/llm_model_hub/backend/byoc/
 bash build_and_push.sh 
 pm2 restart all
