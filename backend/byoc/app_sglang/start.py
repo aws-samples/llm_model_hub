57aaf45b6c1aeb6659e200f84318bd4246aaa01b
River Xie <xiehust@163.com>
2025-02-10 07:27:06 +0000

    update

--- backend/byoc/app_sglang/start.py
+++ backend/byoc/app_sglang/start.py
@@ -14,11 +14,11 @@ if __file__ == "__main__":
     parser.add_argument("--tp-size",type=int,default=1)
     args = parser.parse_args()
 
-    print(f"Running with {args.model_path}")
+    print_highlight(f"Running with {args.model_path}")
 
     execute_shell_command(
         f"""
-python -m sglang.launch_server --model-path {args.model_path} --tp-size {args.tp_size} --dtype {args.dtype} \
+python3 -m sglang.launch_server --model-path {args.model_path} --tp-size {args.tp_size} --dtype {args.dtype} \
 --port 8080 --host 0.0.0.0 --trust-remote-code
 """
     )
